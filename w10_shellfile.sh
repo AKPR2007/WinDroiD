15dc5ae70cb96e80af7f7780618ccdcead3e5434
Exanoff12 <91356410+Exanoff12@users.noreply.github.com>
2021-09-26 19:51:56 +0600

    Update w10_shellfile.sh

--- w10_shellfile.sh
+++ w10_shellfile.sh
@@ -1,6 +1,7 @@
 echo "run windows 10 on termux"
 echo "fresh installation of termux is needed"
 echo "some files used in this process is not owned by me. credits to those files are in github"
+echo "contributors : AKPR2007,Exanoff12"
 echo "starting process"
 apt update && apt upgrade –y
 clear
@@ -8,6 +9,7 @@ pkg install x11-repo
 clear
 pkg install qemu-system-x86_64
 clear
+echo "please accept storage permission"
 termux-setup-storage
 wget -O w10.sh https://is.gd/StartShellFileW10
 echo "settings up is done now run w10.sh"
