1c99bdd5407eede5c85870be5e3b51a0a5078fe1
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2021-09-27 17:43:17 +0600

    Update w10_shellfile.sh

--- w10_shellfile.sh
+++ w10_shellfile.sh
@@ -2,9 +2,9 @@ echo "run windows 10 on termux"
 echo "some files used in this process is not owned by me. credits to those files are in github"
 echo "contributors : AKPR2007,Exanoff12"
 echo "starting process"
-apt update && apt upgrade –y
-pkg install x11-repo
-pkg install qemu-system-x86_64
+apt update && apt upgrade --assume-yes
+pkg install x11-repo --assume-yes
+pkg install qemu-system-x86_64 --assume-yes
 echo "please accept storage permission"
 termux-setup-storage
 wget -O w10.sh https://raw.githubusercontent.com/AKPR2007/W10-in-android_termux/main/w10_start.sh
