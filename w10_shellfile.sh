b2763453aa34115829cdad6664bf3656651ef27c
Exanoff12 <91356410+Exanoff12@users.noreply.github.com>
2021-09-27 12:19:12 +0600

    Update w10_shellfile.sh

--- w10_shellfile.sh
+++ w10_shellfile.sh
@@ -1,5 +1,4 @@
 echo "run windows 10 on termux"
-echo "fresh installation of termux is needed"
 echo "some files used in this process is not owned by me. credits to those files are in github"
 echo "contributors : AKPR2007,Exanoff12"
 echo "starting process"
