900e47f272261688d3abe088e3ea19a47753be45
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2021-09-27 18:08:54 +0600

    No iso directory needed

--- w10_start.sh
+++ w10_start.sh
@@ -1,8 +1,6 @@
 echo "starting vm"
-echo "enter windows 10 iso directory: "
-read dir
 echo "enter vm ram usage limit (type in mb only like if you want to give your vm 1gb ram then type '1024'): "
 read mem
 echo "starting"
-qemu-system-x86_64 -m $mem -cdrom $dir -vnc 127.0.0.1:2
+qemu-system-x86_64 -m $mem -cdrom w10cd.iso -vnc 127.0.0.1:2
 echo "your vm server is ready at vnc 127.0.0.1:2"
