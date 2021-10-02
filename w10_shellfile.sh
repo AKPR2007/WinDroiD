6252d5281f74e92371699b43a4994d8e55fa134e
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2021-10-02 09:13:00 +0600

    Update w10_shellfile.sh

--- w10_shellfile.sh
+++ w10_shellfile.sh
@@ -3,8 +3,8 @@ echo "some files used in this process is not owned by me. credits to those files
 echo "contributors : AKPR2007,Exanoff12"
 echo "starting process"
 apt update && apt upgrade --assume-yes
-pkg install x11-repo --assume-yes
-pkg install qemu-system-x86_64 --assume-yes
+apt-get install x11-repo --assume-yes
+apt-get install qemu-system-x86_64 --assume-yes
 wget -O w10Lite.iso https
 termux-setup-storage
 wget -O w10.sh https://raw.githubusercontent.com/AKPR2007/W10-in-android_termux/main/w10_start.sh
