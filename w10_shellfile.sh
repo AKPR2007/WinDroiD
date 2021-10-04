dc4383d35a4ff78baf44bfd773596e5895aba863
Exanoff12 <91356410+Exanoff12@users.noreply.github.com>
2021-10-04 18:27:43 +0600

    Update w10_shellfile.sh

--- w10_shellfile.sh
+++ w10_shellfile.sh
@@ -2,7 +2,6 @@ echo "run windows 10 on termux"
 echo "some files used in this process is not owned by me. credits to those files are in github"
 echo "contributors : AKPR2007,Exanoff12"
 echo "starting process"
-apt update && apt upgrade --assume-yes
 apt-get install x11-repo --assume-yes
 apt-get install qemu-system-x86_64 --assume-yes
 wget -O w10Lite.iso 'https://firebasestorage.googleapis.com/v0/b/akpr-w10termux-repo.appspot.com/o/w10Lite.iso?alt=media&token=d301b735-4c35-42e0-8346-0d1617301472'
