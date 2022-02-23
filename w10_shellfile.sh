796c3e7f0d8db2f9b7b352666231b498e8b0c687
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2022-02-23 14:03:14 +0600

    Update w10_shellfile.sh

--- w10_shellfile.sh
+++ w10_shellfile.sh
@@ -1,13 +1,13 @@
-echo "run windows 10 on termux"
+echo "run windows 7 on termux"
 echo "some files used in this process is not owned by me. credits to those files are in github"
 echo "contributors : AKPR2007,Exanoff12"
 echo "starting process"
 pkg install x11-repo --assume-yes
 pkg install qemu-system-x86_64 --assume-yes
-wget -O w10Lite.iso 'https://firebasestorage.googleapis.com/v0/b/akpr-w10termux-repo.appspot.com/o/w10Lite.iso?alt=media&token=d301b735-4c35-42e0-8346-0d1617301472'
+wget -O hda.qcow2 'https://firebasestorage.googleapis.com/v0/b/akpr-w10termux-repo.appspot.com/o/w10Lite.iso?alt=media&token=d301b735-4c35-42e0-8346-0d1617301472'
 termux-setup-storage
-wget -O w10 https://raw.githubusercontent.com/AKPR2007/W10-in-android_termux/main/w10_start.sh
-chmod +x w10
+wget -O w7 https://raw.githubusercontent.com/AKPR2007/W10-in-android_termux/main/w10_start.sh
+chmod +x w7
 clear
-echo "settings up is done now run './w10' to start windows 10 !"
+echo "settings up is done now run './w7' to start windows 10 !"
 rm w10setup.sh
