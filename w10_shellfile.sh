62b80db7083bda11b4db41ab3ac3489cb30fd183
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2022-02-23 15:28:21 +0600

    cleaned up mess

--- w10_shellfile.sh
+++ w10_shellfile.sh
@@ -2,11 +2,30 @@ echo "run windows 7 on termux"
 echo "some files used in this process is not owned by me. credits to those files are in github"
 echo "contributors : AKPR2007,Exanoff12"
 echo "starting process"
+sleep 2s
+clear
+echo "updating and upgrading packages"
+sleep 3s
 pkg update && pkg upgrade --assume-yes
+clear
+echo "installating wget"
+sleep 3s
 pkg install wget --assume-yes
+clear
+echo "installating unzip"
+sleep 3s
 pkg install unzip --assume-yes
+clear
+echo "enabling x11 repo"
+sleep 3s
 pkg install x11-repo --assume-yes
+clear
+echo "installating qemu system i386 headless"
+sleep 3s
 pkg install qemu-system-i386-headless --assume-yes
+clear
+echo "setup storage access"
+sleep 3s
 if [ -d "storage" ] 
 then
     echo "internal storage access found. skipping step" 
@@ -15,7 +34,13 @@ else
     termux-setup-storage
 fi
 termux-setup-storage
+clear
+echo "downloading files from AK PR's Server"
+sleep 3s
 wget "link"
+clear
+echo "finalising setup"
+sleep 3s
 unzip w7.zip
 rm w7.zip
 wget -O w7 "link"
