325c5b2aca46d786296b62157b7a2bf777169b92
Exanoff12 <91356410+Exanoff12@users.noreply.github.com>
2022-02-23 08:59:58 +0600

    fixed porting issues

--- w10_start.sh
+++ w10_start.sh
@@ -2,5 +2,5 @@ echo "starting vm"
 echo "enter vm ram usage limit (type in mb only like if you want to give your vm 1gb ram then type '1024'): "
 read mem
 echo "starting"
-qemu-system-x86_64 -m $mem -cdrom w10Lite.iso -vnc 127.0.0.1:2
-echo "your vm server is ready at vnc 127.0.0.1:2"
+qemu-system-x86_64 -m $mem -cdrom w10Lite.iso -vnc 127.0.0.1:7600
+echo "your vm server is ready at vnc 127.0.0.1:7600"
