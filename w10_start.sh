b6fdcd86eb2349974d59cfa2d675d86082380cab
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2022-02-23 15:16:56 +0600

    updated from 10 to 7

--- w10_start.sh
+++ w10_start.sh
@@ -1,6 +1,3 @@
-echo "starting vm"
-echo "enter vm ram usage limit (type in mb only like if you want to give your vm 1gb ram then type '1024'): "
-read mem
 echo "starting"
-qemu-system-x86_64 -m $mem -cdrom w10Lite.iso -vnc 127.0.0.1:7600
-echo "your vm server is ready at vnc 127.0.0.1:7600"
+qemu-system-i386 -hda w7.qcow2 -vnc 127.0.0.1:7600
+echo "your windows server is ready at vnc 127.0.0.1:7600"
