b5347b86da93a8503af0265bb57f89e41e94ae44
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2022-02-25 20:11:11 +0600

    added internet access & shared drive !

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 echo "starting"
-qemu-system-i386 -hda w7.qcow2 -vnc 127.0.0.1:7600
-echo "your windows server is ready at vnc 127.0.0.1:7600"
+sleep 5s
+echo "your windows server is ready at vnc 127.0.0.1:5903"
+qemu-system-i386 -vga vmware -full-screen -hda w7.qcow2 -device rtl8139,netdev=n0 -netdev user,id=n0 -drive file=fat:rw:"Drive" -vnc 127.0.0.1:3
