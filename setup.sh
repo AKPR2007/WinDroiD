3801f2e356889129e3414dcf8d8661812c74f357
Exanoff12 <91356410+Exanoff12@users.noreply.github.com>
2022-03-16 20:12:13 +0600

    update 1.6

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-echo "run windows 7 on termux"
+echo "WinDroiD v1.6"
 echo """
 
  _       ___       ____             _ ____ 
@@ -8,43 +8,55 @@ echo """
 |__/|__/_/_/ /_/_____/_/   \____/_/_____/  
                                            
 """
-echo "contributors : AKPR2007,Exanoff12"
-echo "starting process"
-sleep 4s
+echo "latest version contributors : AKPR2007,Exanoff12"
+echo "preparing to start process..."
+sleep 6s
 clear
 cd ~
 echo "updating and upgrading packages"
 sleep 3s
 yes | apt update && yes | apt upgrade
 clear
-echo "installating wget"
+echo "installating/updating wget"
 sleep 3s
 pkg install wget -y
 clear
-echo "installating unzip"
+echo "installating/updating unzip"
 sleep 3s
 pkg install unzip -y
 clear
-echo "enabling x11 repo"
+echo "enabling/updating x11-repo"
 sleep 3s
 pkg install x11-repo -y
 clear
-echo "installating qemu system i386"
+echo "installating/updating qemu-system-i386"
 sleep 3s
 pkg install qemu-system-i386 -y
 clear
-echo "setup storage access"
+echo "setting up storage access"
 sleep 3s
 if [ -d "storage" ] 
 then
-    echo "internal storage access found. skipping step" 
+    echo "internal storage access already given. skipping step" 
 else
     echo "please give internal storage access"
     termux-setup-storage
 fi
-termux-setup-storage
+sleep 3s
 clear
-echo "downloading files from AK PR's Server"
+echo "fixing nested directories"
+sleep 3s
+if [ -d "storage" ] 
+then
+    echo "fixing successful" 
+else
+    echo "fixing unsuccessful. did you deny the storage permission from the before step?"
+    echo "exiting with error. clear all data of termux from device settings and try again :("
+    exit
+fi
+sleep 3s
+clear
+echo "downloading files from external server"
 sleep 3s
 wget -O w7.zip "https://dl.dropboxusercontent.com/s/iengjqhgoukoxuj/w7.zip?dl=0"
 clear
@@ -63,5 +75,5 @@ cd SharedDrive
 wget -O ThisIsTheSharedFolder "https://raw.githubusercontent.com/AKPR2007/WinDroiD/main/blank"
 cd ~
 clear
-echo "setting up is done now run './win7' to start windows 7!"
+echo "setting up is finished now run './win7' to start windows 7!"
 rm setup.sh
