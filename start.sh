7b27ce84127f73285759cf634f3ef417ebc6d37b
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2022-03-25 18:24:23 +0600

    update 1.7.2 (re) (p)

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 echo "starting"
 sleep 5s
 echo "your windows server is ready at vnc 127.0.0.1:5903"
-qemu-system-i386 -vga vmware -full-screen -hda w7.qcow2 -device rtl8139,netdev=n0 -netdev user,id=n0 -drive file=fat:rw:"storage/shared/WinDroiD/SharedDrive" -vnc 127.0.0.1:3
+qemu-system-i386-headless -vga vmware -full-screen -hda w7.qcow2 -device rtl8139,netdev=n0 -netdev user,id=n0 -drive file=fat:rw:"storage/shared/WinDroiD/SharedDrive" -vnc 127.0.0.1:3
